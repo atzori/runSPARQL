ce8b3a584685bcefb02d9df5de23c793b4f998f3
Maurizio Atzori <atzori@gmail.com>
2019-01-28 01:53:19 +0100

    supporting min and loops via memoization

--- src/main/java/org/webofcode/wfn/runSPARQL.java
+++ src/main/java/org/webofcode/wfn/runSPARQL.java
@@ -1,5 +1,9 @@
-package org.webofcode.wfn;
+/* 
+ *  Copyright 2014-2019. Maurizio Atzori <atzori@unica.it>
+ *
+ */
 
+package org.webofcode.wfn;
 
 /*
 import org.apache.jena.atlas.lib.StrUtils;
@@ -17,12 +21,14 @@ import org.apache.jena.sparql.core.DatasetGraphFactory;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase1;
 import org.apache.jena.sparql.function.FunctionRegistry;
+
+import org.apache.jena.sparql.engine.http.Service;
+import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
 */
 
 import org.apache.jena.sparql.function.*;
 import org.apache.jena.sparql.expr.NodeValue;
-//import org.apache.jena.sparql.engine.http.Service;
-//import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueInteger;
 import org.apache.jena.sparql.function.FunctionRegistry;
 
 
@@ -39,56 +45,92 @@ import org.apache.jena.graph.NodeFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 
+/**
+ * This class implements the runSPARQL function to be used within SPARQL queries in the Apache Fuseki 2 triplestore.
+*/
 public class runSPARQL extends FunctionBase3 {
-   private static Logger log = LoggerFactory.getLogger(runSPARQL.class);
+    private final static boolean CACHE_ENABLED = true;
+
+    static Logger log = LoggerFactory.getLogger(runSPARQL.class);
+    static int nInstances = 0;
+    
+    static Map<String,NodeValue> cache = new ConcurrentHashMap<>();
+
+    static final NodeValue VISITED = NodeValue.makeNode(NodeFactory.createBlankNode("VISITED"));
+    final String TEMPLATE =
+          	"PREFIX wfn: <java:org.webofcode.wfn.>\n"+
+            "SELECT ?result { \n"+
+            "    # bind variables to parameter values\n"+ 
+            "    VALUES (?query ?endpoint ?i0) { ( \n"+
+            "        %querySnippet% \n"+
+            "        %endpoint% \n"+
+            "        %i0% \n"+
+            "    )} \n"+
+            "    # the recursive query \n\n"+
+            "    %queryexec% \n"+
+            "} ORDER BY (?result) \n"; // LIMIT 1 is done by List.get(0) -- in combination with DESC means max
 
 
-   public static void init() {
-        // Register with the global function registry
-        log.info("init called");
-        FunctionRegistry.get().put("http://webofcode.org/wfn/runSPARQL", runSPARQL.class) ;        
+    public static void init() {
+        log.info("Registering function");
+        FunctionRegistry.get().put("http://webofcode.org/wfn/runSPARQL", runSPARQL.class);
     }
-
     public runSPARQL() {
+        nInstances += 1;
+        log.info("Creating instance #{}", nInstances);
     }
-
 
     public NodeValue exec(NodeValue nvQuery, NodeValue nvEndpoint, NodeValue nvInputVar) {
-        log.info("exec called");
+        String querySnippet = nvQuery.toString();
+        String i0 = nvInputVar.toString();
+        String service = nvEndpoint.asUnquotedString();
+        String endpoint = nvEndpoint.toString();
         
-	    String template = 
-              	"PREFIX wfn: <java:org.webofcode.wfn.>\n"+
-	            "SELECT ?result \n"+ 
-	            //"FROM <dataset.rdf>\n"+
-	            "{ \n"+
-	            "    # bind variables to parameter values\n"+ 
-	            "    VALUES (?query ?endpoint ?i0) { ( \n"+
-	            "        %query% \n"+
-	            "        %endpoint% \n"+
-	            "        %i0% \n"+
-	            "    )} \n"+
-	            "    # the recursive query \n\n"+
-	            "    %queryexec% \n"+
-	            "} ORDER BY DESC(?result) \n"; // LIMIT 1 is done by List.get(0) -- in combination with DESC means max
+        log.info("Calling function with i0= {}, service={}", i0, service);
 
-	    String query = template
-		    .replaceAll("%query%",nvQuery.toString())
-		    .replaceAll("%endpoint%",nvEndpoint.toString())
-		    .replaceAll("%i0%",nvInputVar.toString())
-		    .replace("%queryexec%",nvQuery.toString().trim().replaceAll("^\\\"(.*)\\\"$","$1"));
-	    String service = nvEndpoint.asUnquotedString();
-
-            
-        //QueryExecution qe = QueryExecutionFactory.create(query); // local call not working without specifying a dataset
-
+        String query = TEMPLATE
+            .replaceAll("%querySnippet%", querySnippet)
+            .replaceAll("%endpoint%", endpoint)
+            .replaceAll("%i0%", i0)
+            .replace("%queryexec%", querySnippet.trim().replaceAll("^\\\"(.*)\\\"$","$1"));
         
-        log.info("i0 = " + nvInputVar.toString());
-        log.info("service = " + service);
+        NodeValue result = null;  
+
+        // try to avoid loops    
+        String key = String.format("%s|%s", i0, query); // endpoint
+        if (CACHE_ENABLED) {
+            if(cache.containsKey(key)) {
+                result = cache.get(key); 
+                log.info("Loop found for i0={}, result={}", i0, result);
+                return result==VISITED? nodeNone(): result; 
+            } else 
+                cache.put(key, VISITED); // mark as "visited (but not resolved yet)" 
+        }
+        
+        List<RDFNode> solutions = executeQuery(query, service);
+        log.info("Result size for i0={} was {}, solutions= {}", i0, solutions.size(), solutions);
+        
+        if (solutions.size()>0) {
+            RDFNode node = solutions.get(0); // only the first solution is used
+            result =(node == null) ? nodeNone() : NodeValue.makeNode(node.asNode()); // check why node can/could(?) be null
+        } else {
+	        log.warn("Resultset was empty");
+		    result = nodeNone(); //nvNothing; // Expr.NONE; //NodeValue.makeString("no result!");
+        }
+
+	    log.info("Result was {}", result);
+	    if(CACHE_ENABLED) cache.put(key, result);
+        return result;        
+
+    }
+    
+    private static List<RDFNode> executeQuery(String query, String service) {
+    
+        //QueryExecution qe = QueryExecutionFactory.create(query); // local call not working without specifying a dataset        
         //log.info(""+ARQConstants.sysCurrentDataset ); //symDatasetDefaultGraphs.toString()) ;
 
 
@@ -105,11 +147,11 @@ public class runSPARQL extends FunctionBase3 {
 
         }*/
 
+
         /*
-            An HttpClient is necessary since default accept only a limited number of connections (no more than 5)
+            An HttpClient is necessary since default client only accepts a limited number of connections (no more than 5)
             https://stackoverflow.com/questions/49661698/how-can-i-change-the-number-of-connections-par-route-in-jena-more-than-5
         */
-
         HttpClient client = HttpClientBuilder.create().build();
 	    
 	    try (QueryExecution qe = 
@@ -121,29 +163,17 @@ public class runSPARQL extends FunctionBase3 {
             //rs = ResultSetFactory.copyResults(rs) ;
             solutions = resultSet2List(rs);            
         }
-        
-        log.info("result size was " + solutions.size());
-        if (solutions.size()>0) {
-            RDFNode node = solutions.get(0); // only the first solution is used
-
-		    log.info("result was " + node);
-		    if (node == null) 
-		        return nodeNone(); //nvNothing; //Expr.NONE; //throw new RuntimeException("runSPARQL: node null");
-	        return NodeValue.makeNode(node.asNode());
-                    
-        } else {
-	        log.info("resultset was empty");
-		    return nodeNone(); //nvNothing; // Expr.NONE; //NodeValue.makeString("no result!");        
-        }
-
+        return solutions;
+    
     }
     
-    @SuppressWarnings( "deprecation" )
+    //@SuppressWarnings( "deprecation" )
     private static NodeValue nodeNone() {
         //return (NodeValue) NodeValue.NONE;
         //return NodeValue.nvNothing; // equivalent to: NodeValue.makeNode(NodeFactory.createBlankNode(strForUnNode)) 
-        return NodeValue.makeNode(NodeFactory.createBlankNode("noResult"));
         //return NodeValue.NONE.eval(null,null);
+        return NodeValue.makeNode(NodeFactory.createBlankNode("noResult")); // this is working
+        //return new NodeValueInteger(-999);
     }
     
     /** convert a ResultSet with column "result" into a List of RDFNode */
