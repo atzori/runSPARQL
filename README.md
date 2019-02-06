f113556ab59801ce32dbd2ad698738ab52d786ee
Maurizio Atzori <atzori@users.noreply.github.com>
2019-02-06 14:10:41 +0100

    cleaned version of ICSC14 runSPARQL with bufixes, supporting multiple parameters, logging, better defaults (#1)
    cleaned runSPARQL version

--- README.md
+++ README.md
@@ -13,62 +13,88 @@ Our work can be found in:
  * [Computing Recursive SPARQL Queries](https://doi.org/10.1109/ICSC.2014.54). Maurizio Atzori. _8th IEEE International Conference on Semantic Computing_ (2014)
 
 
-Name space
-----------
-Officially, we suggest the use of the following namespace:
-
-    wfn: <http://webofcode.org/wfn/>
-
-Function will be called with `wfn:runSPARQL`. Please note that if function is not registered, you must use `PREFIX wfn: <java:org.webofcode.wfn.>` as in the examples section.
 
 
 Install and compile *(tested with Fuseki 3.8.0)*
 -------------------
 1. ensure you have OpenJDK 8 or later correctly installed
 2. download and extract [Apache Fuseki 2](https://jena.apache.org/download/#apache-jena-fuseki) on directory `./fuseki`
-3. compile runSPARQL Java source code: `./compile`
-4. run fuseki: `./run-fuseki` *(this will use settings in `./config/config.ttl`)*
+3. compile *runSPARQL* Java source code: `./compile`
+4. run fuseki using testing settings: `./run-fuseki`. The command will use the the following default configurations:
+    - fuseki server settings in `config/config.ttl` 
+    - initial data in `config/dataset.ttl` 
+    - log4j settings in `config/log4j.properties`
+5. go to: [http://127.0.0.1:3030](http://127.0.0.1:3030) and run a recursive query (see below for examples)
 
 
 Usage
 -----
+Officially, we suggest the use of the following namespace:
 
-`runSPARQL(query, endpoint, initValue)`
+    PREFIX wfn: <http://webofcode.org/wfn/>
+
+The function is called with `wfn:runSPARQL`. Please note that if function is not registered, you must use `PREFIX wfn: <java:org.webofcode.wfn.>`.
+
+The usage is the following:
+
+     wfn:runSPARQL(query, endpoint [,i0, i1, ...])
+     
+where: 
+  - `query` is a SPARQL query fragment that will be executed by the `runSPARQL` function
+  - `endpoint` is a url pointing to the SPARQL endpoint that must be used to run the SPARQL query
+  - `i0`, `i1`, etc. are optional parameter to be used in the SPARQL query 
+
+
+When called, the `runSPARQL` custom function will execute `query` (that may contain references to runSPARQL itself) against `endpoint`, where variables `?i0`, `?i1`, etc are binded to the values passed to `runSPARQL`.
 
 
 Examples
 --------
 
 ### Computing the Factorial
-The following is an example of recursive SPARQL query that computes the factorial of 3: 
-```
-PREFIX wfn: <http://webofcode.org/wfn/>  # alternatively: PREFIX wfn: <java:org.webofcode.wfn.>
+The following is an example of recursive SPARQL query that computes the factorial of 3, returning `"6"^^xsd:integer`: 
 
-SELECT ?result 
-{ 
-        # bind variables to parameter values 
-        VALUES (?query ?endpoint) { ( 
-                "BIND ( IF(?i0 <= 0, 1, ?i0 * wfn:runSPARQL(?query,?endpoint, ?i0 -1)) AS ?result)" 
-                "http://127.0.0.1:3030/ds/sparql"
+    PREFIX wfn: <http://webofcode.org/wfn/> 
+
+    SELECT ?result { 
+            # bind variables to parameter values 
+            VALUES (?query ?endpoint) { ( 
+                    "BIND ( IF(?i0 <= 0, 1, ?i0 * wfn:runSPARQL(?query,?endpoint, ?i0 -1)) AS ?result)" 
+                    "http://127.0.0.1:3030/ds/sparql"
+            )}
+      
+       
+            # actual call of the recursive query 
+            BIND( wfn:runSPARQL(?query,?endpoint,3) AS ?result)
+    }
+
+
+At the first iteration, runSPARQL will run the following:
+
+    PREFIX wfn: <java:org.webofcode.wfn.>
+    SELECT ?result {
+        # bind variables to parameter values
+        VALUES (?query ?endpoint ?i0) { (
+            "BIND ( IF(?i0 <= 0, 1, ?i0 * wfn:runSPARQL(?query,?endpoint, ?i0 -1)) AS ?result)"
+            "http://127.0.0.1:3030/ds/sparql"
+            3
         )}
-  
-   
-        # actual call of the recursive query 
-        BIND( wfn:runSPARQL(?query,?endpoint,3) AS ?result)
-}
-```
+        
+        # the recursive query
+        BIND ( IF(?i0 <= 0, 1, ?i0 * wfn:runSPARQL(?query,?endpoint, ?i0 -1)) AS ?result)
+        FILTER (!isBlank(?result))
+    } LIMIT 1
 
-### Graph search: node distance
+In the following iteration `?i0` will be then set to `2`, ..., until `0` is reached and recursion stops as per the base case specified in `?query`.
+
+
+### Graph search
 An example of recursive SPARQL query that computes the distance between two nodes ([dbo:PopulatedPlace](http://dbpedia.org/ontology/PopulatedPlace) and [dbo:Village](http://dbpedia.org/ontology/Village)) in a hierarchy: 
 
 ```
-PREFIX wfn: <http://webofcode.org/wfn/>  # alternatively: PREFIX wfn: <java:org.webofcode.wfn.>
-PREFIX db: <http://dbpedia.org/>
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX wfn: <http://webofcode.org/wfn/>
 
-SELECT ?result 
-{ 
+SELECT ?result { 
         # bind variables to parameter values 
         VALUES (?query ?endpoint) { ( 
                 "?i0 <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?next. BIND( IF(?next = <http://dbpedia.org/ontology/PopulatedPlace>, 1 , 1 + wfn:runSPARQL(?query, ?endpoint, ?next)) AS ?result)" 
@@ -80,17 +106,34 @@ SELECT ?result
 } 
 ```
 
-The call of `runSPARQL` will generate another SPARQL query, recursively calling `runSPARQL`, similar to the following one:
-```
-PREFIX wfn: <java:org.webofcode.wfn.>
-PREFIX db: <http://dbpedia.org/>
-PREFIX dbo: <http://dbpedia.org/ontology/>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+Note that the default dataset is used (in `config/dataset.ttl`) where [dbr:Village](http://dbpedia.org/resource/Village) and [dbr:PopulatedPlace](http://dbpedia.org/resource/PopulatedPlace) have distance 2 like in DBpedia: 
 
-SELECT ?result 
-{ 
-   <http://dbpedia.org/ontology/Village> <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?next. 
-   BIND( IF(?next = <http://dbpedia.org/ontology/Place>, 1 , ?next) AS ?result)
-} LIMIT 1
-```
+    dbr:Village -> dbr:Settlement -> dbr:PopulatedPlace
+
+The call of `runSPARQL` will return `"2"^^xsd:integer`, by generating another SPARQL query, recursively calling `runSPARQL`, that in its first iteration is similar to the following one:
+
+    PREFIX wfn: <java:org.webofcode.wfn.>
+    SELECT ?result {
+        # bind variables to parameter values
+        VALUES (?query ?endpoint ?i0) { (
+            "?i0 <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?next. BIND( IF(?next = <http://dbpedia.org/ontology/PopulatedPlace>, 1 , 1 + wfn:runSPARQL(?query, ?endpoint, ?next)) AS ?result)"
+            "http://127.0.0.1:3030/ds/sparql"
+            <http://dbpedia.org/ontology/Village>
+         )}
+        
+        # the recursive query
+        ?i0 <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?next. 
+        BIND( IF(?next = <http://dbpedia.org/ontology/PopulatedPlace>, 1 , 1 + wfn:runSPARQL(?query, ?endpoint, ?next)) AS ?result)
+        FILTER (!isBlank(?result))
+    } LIMIT 1
+
+
+For reading purposes only, the above can be **loosely** rewritten to the following:
+
+    SELECT ?result 
+    { 
+       <http://dbpedia.org/ontology/Village> <http://www.w3.org/2000/01/rdf-schema#subClassOf> ?next. 
+       BIND( IF(?next = <http://dbpedia.org/ontology/PopulatedPlace>, 1 , ?next) AS ?result)  # otherwise use recursion
+    } LIMIT 1
+
 
--- src/main/java/org/webofcode/wfn/runSPARQL.java
+++ src/main/java/org/webofcode/wfn/runSPARQL.java
@@ -1,28 +1,13 @@
+/* 
+ *  Copyright 2014-2019. Maurizio Atzori <atzori@unica.it>
+ *
+ */
+ 
 package org.webofcode.wfn;
-
-
-/*
-import org.apache.jena.atlas.lib.StrUtils;
-import org.apache.jena.atlas.logging.LogCtl;
-import org.apache.jena.fuseki.FusekiLib;
-import org.apache.jena.fuseki.embedded.FusekiServer;
-import org.apache.jena.fuseki.system.FusekiLogging;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
-import org.apache.jena.rdfconnection.RDFConnection;
-import org.apache.jena.rdfconnection.RDFConnectionFactory;
-import org.apache.jena.sparql.core.DatasetGraph;
-import org.apache.jena.sparql.core.DatasetGraphFactory;
-import org.apache.jena.sparql.expr.NodeValue;
-import org.apache.jena.sparql.function.FunctionBase1;
-import org.apache.jena.sparql.function.FunctionRegistry;
-*/
 
 import org.apache.jena.sparql.function.*;
 import org.apache.jena.sparql.expr.NodeValue;
-//import org.apache.jena.sparql.engine.http.Service;
-//import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+import org.apache.jena.sparql.expr.nodevalue.NodeValueInteger;
 import org.apache.jena.sparql.function.FunctionRegistry;
 
 
@@ -39,111 +24,123 @@ import org.apache.jena.graph.NodeFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.*;
+import static java.util.stream.Collectors.*;
+
+import org.apache.jena.atlas.lib.Lib;
+import org.apache.jena.query.QueryBuildException;
+import org.apache.jena.sparql.ARQInternalErrorException;
+import org.apache.jena.sparql.expr.ExprEvalException;
+import org.apache.jena.sparql.expr.ExprList;
+
+/**
+ * This class implements the runSPARQL function to be used within SPARQL queries in the Apache Fuseki 2 triplestore.
+*/
+public class runSPARQL extends FunctionBase {
+    static Logger log = LoggerFactory.getLogger(runSPARQL.class);
+    
+    static final String FN_NAME = "http://webofcode.org/wfn/runSPARQL";
+    static String TEMPLATE;
 
 
-public class runSPARQL extends FunctionBase3 {
-   private static Logger log = LoggerFactory.getLogger(runSPARQL.class);
-
-
-   public static void init() {
-        // Register with the global function registry
-        log.info("init called");
-        FunctionRegistry.get().put("http://webofcode.org/wfn/runSPARQL", runSPARQL.class) ;        
-    }
-
-    public runSPARQL() {
-    }
-
-
-    public NodeValue exec(NodeValue nvQuery, NodeValue nvEndpoint, NodeValue nvInputVar) {
-        log.info("exec called");
+    public static void init() {
+        log.info("Registering function {}", FN_NAME);
+        FunctionRegistry.get().put(FN_NAME, runSPARQL.class);
         
-	    String template = 
-              	"PREFIX wfn: <java:org.webofcode.wfn.>\n"+
-	            "SELECT ?result \n"+ 
-	            //"FROM <dataset.rdf>\n"+
-	            "{ \n"+
-	            "    # bind variables to parameter values\n"+ 
-	            "    VALUES (?query ?endpoint ?i0) { ( \n"+
-	            "        %query% \n"+
-	            "        %endpoint% \n"+
-	            "        %i0% \n"+
-	            "    )} \n"+
-	            "    # the recursive query \n\n"+
-	            "    %queryexec% \n"+
-	            "} ORDER BY DESC(?result) \n"; // LIMIT 1 is done by List.get(0) -- in combination with DESC means max
+        // load sparql template file     
+        try {
+            TEMPLATE = String.join("\n", Files.readAllLines(Paths.get(System.getProperty("sparql_template"))));
+        } catch(Exception e) {
+            log.error("Error while reading sparql template");
+        }
+        //log.debug("Template set to {}", TEMPLATE);
+        
+    }
 
-	    String query = template
-		    .replaceAll("%query%",nvQuery.toString())
-		    .replaceAll("%endpoint%",nvEndpoint.toString())
-		    .replaceAll("%i0%",nvInputVar.toString())
-		    .replace("%queryexec%",nvQuery.toString().trim().replaceAll("^\\\"(.*)\\\"$","$1"));
-	    String service = nvEndpoint.asUnquotedString();
+    @Override
+    public void checkBuild(String uri, ExprList args) { 
+        if ( args.size() < 2 )
+            throw new QueryBuildException("Function '"+Lib.className(this)+"' takes at least 2 arguments (a query snippet and an endpoint url)") ;
+    }
 
+    public NodeValue exec(List<NodeValue> args) { 
+    
+        if ( args == null )
+            // The contract on the function interface is that this should not happen.
+            throw new ARQInternalErrorException(Lib.className(this)+": Null args list") ;
+        
+        if ( args.size() < 2 )
+            throw new ExprEvalException(Lib.className(this)+": Wrong number of arguments: Wanted 1+, got "+args.size()) ;
+        
+
+        String querySnippet = args.get(0).toString();
+        String endpoint = args.get(1).toString();
+        
+        List<NodeValue> fnArgs = new ArrayList<>(args);
+        fnArgs.remove(0);
+        fnArgs.remove(0);
+        log.info("Calling with {}", fnArgs);
             
-        //QueryExecution qe = QueryExecutionFactory.create(query); // local call not working without specifying a dataset
 
+        String varNames = IntStream.range(0,fnArgs.size()).mapToObj(i -> "?i"+i).collect(Collectors.joining(" "));
+        String varValues = args.stream().map(e -> e.toString()).collect(Collectors.joining("\n"));
+
+        String bindings = String.format("VALUES (?query ?endpoint %s) { (\n%s\n)}", varNames, varValues);
         
-        log.info("i0 = " + nvInputVar.toString());
-        log.info("service = " + service);
-        //log.info(""+ARQConstants.sysCurrentDataset ); //symDatasetDefaultGraphs.toString()) ;
+        String query = TEMPLATE
+            .replace("%query_snippet%", querySnippet.trim().replaceAll("^\\\"(.*)\\\"$","$1"))
+            .replace("%bindings%", bindings);
+                    
+                
+        NodeValue result = null;  
 
 
+        List<RDFNode> solutions = executeQuery(query, args.get(1).asUnquotedString());
+        
+        if (solutions.size()>0) {
+            RDFNode node = solutions.get(0); // only the first solution is used
+            result =(node == null) ? nodeNone() : NodeValue.makeNode(node.asNode()); // check why node can/could(?) be null
+        } else {
+	        log.warn("Resultset was empty");
+		    result = nodeNone(); //nvNothing; // Expr.NONE; //NodeValue.makeString("no result!");
+        }
+
+	    //log.info("Result was {}", result);
+	    log.info("Result for {} was {}, solutions={}", fnArgs, result, solutions);
+        return result;        
+
+    }
+    
+    private static List<RDFNode> executeQuery(String query, String service) {
 	    List<RDFNode> solutions = null;
-       /* try ( RDFConnection conn = RDFConnectionFactory.connect("http://127.0.0.1:3030/ds") ) {
-
-            log.info("within try x1");
-
-            ResultSet rs = conn.query(query).execSelect() ;
-            log.info("within try x2");
-            solutions = resultSet2List(rs);
-            //return ResultSetFactory.copyResults(rs) ;
-
-
-        }*/
 
         /*
-            An HttpClient is necessary since default accept only a limited number of connections (no more than 5)
+            An HttpClient is necessary since default client only accepts a limited number of connections (no more than 5)
             https://stackoverflow.com/questions/49661698/how-can-i-change-the-number-of-connections-par-route-in-jena-more-than-5
         */
-
         HttpClient client = HttpClientBuilder.create().build();
+	    
+	    log.debug("Running query\n"+query);
 	    
 	    try (QueryExecution qe = 
 	            QueryExecutionFactory.sparqlService(service, query, client)
-	            //QueryExecutionFactory.create(query, Dataset.getDefaultModel()) 
 	      ) {
 	      
             ResultSet rs = qe.execSelect(); // sparql query is actually executed remotely on the endpoint
             //rs = ResultSetFactory.copyResults(rs) ;
             solutions = resultSet2List(rs);            
         }
-        
-        log.info("result size was " + solutions.size());
-        if (solutions.size()>0) {
-            RDFNode node = solutions.get(0); // only the first solution is used
-
-		    log.info("result was " + node);
-		    if (node == null) 
-		        return nodeNone(); //nvNothing; //Expr.NONE; //throw new RuntimeException("runSPARQL: node null");
-	        return NodeValue.makeNode(node.asNode());
-                    
-        } else {
-	        log.info("resultset was empty");
-		    return nodeNone(); //nvNothing; // Expr.NONE; //NodeValue.makeString("no result!");        
-        }
-
+        return solutions;
+    
     }
     
-    @SuppressWarnings( "deprecation" )
+    //@SuppressWarnings( "deprecation" )
     private static NodeValue nodeNone() {
-        //return (NodeValue) NodeValue.NONE;
-        //return NodeValue.nvNothing; // equivalent to: NodeValue.makeNode(NodeFactory.createBlankNode(strForUnNode)) 
-        return NodeValue.makeNode(NodeFactory.createBlankNode("noResult"));
-        //return NodeValue.NONE.eval(null,null);
+        return NodeValue.makeNode(NodeFactory.createBlankNode("noResult")); 
     }
     
     /** convert a ResultSet with column "result" into a List of RDFNode */
